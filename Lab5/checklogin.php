6e0a41f7c307d50a414889ac7e3b25e60ee1cdee
Zackyization <zackyization@gmail.com>
2018-10-05 07:25:10 +0800

    add some files for lab5

---
+++ Lab5/checklogin.php
@@ -0,0 +1,21 @@
+<?php
+    $u = $_POST['uname']; //name of the username field is 'uname'
+    $p = $_POST['pw']; //name of the password field is 'pw'
+
+    $conn = mysqli_connect("127.0.0.1", "root", "", "db185563R");
+
+    $sql = "SELECT * FROM userinfo WHERE name = '$u' and passwd='$p'";
+    $search_result = mysqli_query($conn,$sql); //search table NOW!
+
+    //Return the number of rows in search result
+    $userfound = mysqli_num_rows($search_result);
+
+    if ($userfound >= 1) {
+        //User record is found in the userinfo table
+        header("Location:success.html"); //go to success.html
+    }
+    else {
+        //User record is NOT found in the userinfo table
+        header("Location:login.php");
+    }
+?>
---
+++ Lab5/login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Login Page - 185563R</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+    <form action="checklogin.php">
+        <label>Username:</label>
+        <input name="uname" type="text">
+
+        <br>
+
+        <label>Password:</label>
+        <input name="pw" type="password">
+
+        <br>
+
+        <button type="submit">Login</button>
+    </form>
+</body>
+
+</html>
