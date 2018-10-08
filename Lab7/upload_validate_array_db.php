92fb09f799f48dcbd81305c5a83a2ca8f933af06
Zackyization <zackyization@gmail.com>
2018-10-09 00:26:36 +0800

    fix exercises for lab7

--- Lab7/upload_validate_array_db.php
+++ Lab7/upload_validate_array_db.php
@@ -5,11 +5,10 @@
     echo "The file type is " . $_FILES["fileToUpload"]["type"] . "<br>";
 
     function doSaveData($usr, $tgt) {
-        $conn = mysqli_connect("127.0.0.1", "root", "", "db185563r");
+        $conn = mysqli_connect("127.0.0.1", "root", "", "db185563R");
 
         //Write an SQL statement to extract all countries from country table
-        $sql = "INSERT INTO user_upload('username', 'uploaded_file') VALUES ('$tgt', '$usr')";
-
+        $sql = "INSERT INTO user_upload(username, uploaded_file) VALUES ('$usr', '$tgt')";
         //execute the SQL statement
         $result = mysqli_query($conn, $sql);
 
@@ -29,19 +28,18 @@
     //Add timestamp at the filename
     $target = "uploaded_files/" . $timestamp . "_" . $_FILES["fileToUpload"]["name"];
 
-    
     $allowedType = array("image/gif", "image/jpeg", "image/jpg", "image/png");
     if (in_array($_FILES["fileToUpload"]["type"], $allowedType)) {
         echo "File type is acceptable <br>"; //proceed to upload
         $result = move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target);
 
         if ($result) {
-            echo "Upload Success!";
+            echo "File Upload Success! <br>";
             $username = $_POST['username'];
             doSaveData($username, $target);
         }
         else {
-            echo "Upload FAILED!";
+            echo "File Upload FAILED!";
         }
     }
     else {
