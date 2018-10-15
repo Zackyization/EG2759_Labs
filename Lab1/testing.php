0d0f549307c8fd56c0cfd584810a66b684dd14ba
Zildjian Garcia <zackyization@gmail.com>
2018-10-15 10:58:26 +0800

    Add Lab1

---
+++ Lab1/testing.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Lab 1</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+    <h1>This is Lab1</h1>
+
+<?php
+// $name = "Richard";
+$name = "Richard Stallman";
+
+
+echo "1. The free software movement was launched by <b>Richard</b> <br>";
+echo "2. The free software movement was launched by <b>$name</b> <br>";
+echo '3. The free software movement was launched by <b>$name</b> <br>';
+echo "4. The <i>free software</i> movement was launched by <b>$name<sup>#</sup></b> <br>";
+
+?>
+</body>
+
+</html>
