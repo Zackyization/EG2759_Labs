c35856420de2d5700f2c5f408a337c769ec584a2
Zackyization <zackyization@gmail.com>
2018-10-29 10:26:46 +0800

    update lab3

--- Lab3/lowercase.php
+++ Lab3/lowercase.php
@@ -1,4 +1,20 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Lowercase exercise</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
 <?php
+echo "<b>What is ord() function?</b>";
+echo "<br>";
+echo "the ASCII of 'a' is " . ord('a');
+echo "<br>";
+echo "<br>";
+
 function is_str_all_lowercase($text) {
     for ($sc = 0; $sc < strlen($text); $sc++) {
         if (ord($text[$sc]) >= ord('A') && ord($text[$sc]) <= ord('Z')) {
@@ -17,5 +33,17 @@ if (is_str_all_lowercase($test_text)) {
     echo $test_text . ": is ALL LOWERCASE!!";
 } else {
     echo $test_text . ": contains UPPERCASE characters.";
+
+    echo "<br>";
+
+    echo "The uppercase characters in the string of '$test_text': ";
+    $foo = str_split($test_text);
+    foreach($foo as $char) {
+        if (ctype_upper($char)) {
+            echo $char;
+        }
+    }
 }
 ?>
+</body>
+</html>
